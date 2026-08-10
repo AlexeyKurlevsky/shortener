787231badeb66c4b42598b23b3e290716cf37cf4
AlexeyKurlevsky <kurlevsky@gmail.com>
2026-08-11 00:25:22 +0300

    remove

--- internal/storage/migrations/000002_create_users_table.down.sql
+++
@@ -1,8 +0,0 @@
--- Удаляем таблицу users вместе с зависимостями (внешний ключ будет удалён автоматически)
-DROP TABLE IF EXISTS users CASCADE;
-
--- Удаляем колонку user_id из таблицы urls
-ALTER TABLE urls DROP COLUMN IF EXISTS user_id;
-
--- Удаляем индекс (если он остался)
-DROP INDEX IF EXISTS idx_urls_user_id;
