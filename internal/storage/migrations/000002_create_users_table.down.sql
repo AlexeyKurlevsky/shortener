6ece83559d678d8ec1af0c959137d058580daa61
AlexeyKurlevsky <kurlevsky@gmail.com>
2026-08-11 00:10:36 +0300

    upd

--- internal/storage/migrations/000002_create_users_table.down.sql
+++ internal/storage/migrations/000002_create_users_table.down.sql
@@ -4,8 +4,5 @@ ALTER TABLE urls DROP CONSTRAINT IF EXISTS fk_urls_user_id CASCADE;
 -- Удаляем индекс
 DROP INDEX IF EXISTS idx_urls_user_id;
 
--- Теперь можно удалить таблицу users
-DROP TABLE IF EXISTS users CASCADE;
-
 -- Удаляем колонку user_id
 ALTER TABLE urls DROP COLUMN IF EXISTS user_id;
