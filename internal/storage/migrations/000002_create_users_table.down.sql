9e6a567561ab11b7e35c779a82b9bcd680b177fe
AlexeyKurlevsky <kurlevsky@gmail.com>
2026-08-11 00:06:38 +0300

    upd

--- internal/storage/migrations/000002_create_users_table.down.sql
+++ internal/storage/migrations/000002_create_users_table.down.sql
@@ -1,11 +1,11 @@
--- Удаляем внешний ключ
-ALTER TABLE urls DROP CONSTRAINT IF EXISTS fk_urls_user_id;
+-- Удаляем внешний ключ с каскадным удалением зависимостей
+ALTER TABLE urls DROP CONSTRAINT IF EXISTS fk_urls_user_id CASCADE;
 
 -- Удаляем индекс
 DROP INDEX IF EXISTS idx_urls_user_id;
 
--- Удаляем таблицу users
-DROP TABLE IF EXISTS users;
+-- Теперь можно удалить таблицу users
+DROP TABLE IF EXISTS users CASCADE;
 
 -- Удаляем колонку user_id
 ALTER TABLE urls DROP COLUMN IF EXISTS user_id;
